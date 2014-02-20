3b888e462fd918997671e816f0d141f11aa6f65e
Ben Zimmerman <benzimmerman@BenZ-L060.local>
2014-02-19 23:05:03 -0500

    Changing model to build sentences. Changing template to just show the sentence

--- js/app.js
+++ js/app.js
@@ -15,7 +15,31 @@ App.Router.map(function() {
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-  	return ['hi'];
-    //return jira.getFilterResults(15897);
+  	// determine whether plural or singular
+  	var singOrPlural = Math.floor((Math.random()*2)+1)
+  	var plurality = "singular";
+  	if (singOrPlural == 2)
+  		plurality = "plural";
+  	var subject = nouns[plurality][Math.floor(Math.random()*nouns[plurality].length)];
+  	var verb = verbs[plurality][Math.floor(Math.random()*verbs[plurality].length)];
+  	var ending = prepositions[Math.floor(Math.random()*prepositions.length)];
+  	var useDO = Math.floor((Math.random()*2));
+  	if (useDO)
+  		ending = nouns[plurality][Math.floor(Math.random()*nouns[plurality].length)];
+  	return subject + ' ' + verb + ' ' + ending;
   }
 });
+
+
+var nouns = {
+	"singular" : ['your dad', 'my girlfriend', 'the man', 'life', 'paper', 'iPad'],
+	"plural" : ['kids', 'ghosts', 'bands', 'car keys', 'cell phones']
+};
+
+var verbs = {
+	"singular" : ['screams', 'tastes like', 'drinks', 'hates'],
+	"plural" : ['love', 'destroy', 'stroke', 'smash', 'run', 'walk']
+};
+
+var prepositions = 
+	['at kids', 'down the alley', 'with all their heart', 'over the hill', 'around the bend'];
